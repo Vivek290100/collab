421f2780723ef6e49f0f6e28573e80406a7cf636
vivek <vivekv290100@gmail.com>
2024-10-11 19:31:45 +0530

    sample server running successfully

--- collab-backend/server.js
+++ collab-backend/server.js
@@ -1,1 +1,19 @@
-// new
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/api/data', (req, res) => {
+    // const data = req.body;
+    // res.status(200).json({ message: 'Data received', data });
+    res.send("Hiiiiiiiiiiiiii!")
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on port "http://localhost:${PORT}"`);
+});
